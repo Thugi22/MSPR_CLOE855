476221b307f814d3db5f86d2dca326f4ae57372f
Thuginthan <138033410+Thugi22@users.noreply.github.com>
2024-06-03 15:42:27 +0200

    Ajout du fichier app.py avec la route /fiche_nom/ protégée

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify, abort
+from functools import wraps
+import sqlite3
+
+app = Flask(__name__)
+
+# Identifiants de l'utilisateur
+USER_CREDENTIALS = {
+    'user': '12345'
+}
+
+def check_user_auth(username, password):
+    return USER_CREDENTIALS.get(username) == password
+
+def user_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_user_auth(auth.username, auth.password):
+            return jsonify({"message": "Authentication required"}), 401
+        return f(*args, **kwargs)
+    return decorated_function
+
+@app.route('/fiche_nom/<name>', methods=['GET'])
+@user_required
+def get_client_by_name(name):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM clients WHERE name = ?", (name,))
+    client = cursor.fetchone()
+    conn.close()
+    
+    if client:
+        return jsonify({
+            'id': client[0],
+            'name': client[1],
+            'other_info': client[2]
+        })
+    else:
+        return abort(404, description="Client not found")
+
+if __name__ == '__main__':
+    app.run(debug=True)
